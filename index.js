caef0e2292ec7622a08ec0d39330ead70079498a
Mitch4444 <eye8adog@gmail.com>
2019-03-07 12:22:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ function watchFormSubmit() {
     });
   }
   
-  
+  //link the api url to retrieve data
   function getBeersFromApi(abv){
     const url = `https://api.punkapi.com/v2/beers?abv_gt=${abv}`;
-  
+  // if everything is correct this will retrieve the given data
     fetch(url)
       .then(res => {
         if (res.ok){
           return res.json();
         }
-        throw new Error(res.statusText);
+        throw new Error(res.statusText); // give errors such as 400 
       })
       .then(results => {
         if(!results.length){
@@ -38,20 +38,21 @@ function watchFormSubmit() {
   }
   function input() {
       var input = document.getElementById("abv").value;
+      // uses user input to adjust what to return
   }
   
-  
+  //return the results
   function decorateResults(results){
     const decorated = results.map(result => {
       return decorateSingleResult(result);
     })
     $('#results').html(decorated);
   }
-  
+  // the layout on how the results should look
   function decorateSingleResult(result){
     return `
       <li>
-        <h2>${result.name}:</h2>
+        <h2 class="results">${result.name}:</h2>
         <p class= "tagline">${result.tagline}</p>
           <h3>Description:</h3>
           <p>${result.description}</p>
